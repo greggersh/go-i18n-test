32d2a04edaf389300ca30a9bdde30f3b31942601
Greg Gershman <greg@shelrick.com>
2014-10-15 09:37:36 -0400

    Test fallback.

--- main.go
+++ main.go
@@ -8,12 +8,16 @@ import (
 func main() {
 	i18n.MustLoadTranslationFile("en-US.all.json")
 	i18n.MustLoadTranslationFile("es-MX.all.json")
-	Ten, err := i18n.Tfunc("en-US")
+  fmt.Println("Test English")
+	enT, err := i18n.Tfunc("en-US")
 	if err == nil {
-		fmt.Printf(Ten("hello_world") + "\n")
+		fmt.Println(enT("hello_world"))
 	}
-	Tes, err := i18n.Tfunc("es-MX")
+  fmt.Println("Test Spanish")
+	esT, err := i18n.Tfunc("es-MX")
 	if err == nil {
-		fmt.Printf(Tes("hello_world") + "\n")
+		fmt.Println(esT("hello_world"))
+    fmt.Println("Test fallback (key is not in ES dictionary, but is in English)")
+    fmt.Println(esT("fallback"))
 	}
 }
