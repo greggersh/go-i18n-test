84130436b3814f63c982a9b6048cc29af7f3a420
Greg Gershman <greg@shelrick.com>
2014-10-14 20:45:10 -0400

    Initial commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import "github.com/nicksnyder/go-i18n/i18n"
+import "fmt"
+
+func main() {
+	i18n.MustLoadTranslationFile("en-US.all.json")
+	i18n.MustLoadTranslationFile("es-MX.all.json")
+	Ten, err := i18n.Tfunc("en-US")
+	if err == nil {
+		fmt.Printf(Ten("hello_world") + "\n")
+	}
+	Tes, err := i18n.Tfunc("es-MX")
+	if err == nil {
+		fmt.Printf(Tes("hello_world") + "\n")
+	}
+}
